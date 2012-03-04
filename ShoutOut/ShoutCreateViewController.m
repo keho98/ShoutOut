547f7e6edc800c3ef818278608194f5961e7ade5
Forrest Browning <fandjgames@yahoo.com>
2012-03-03 21:10:59 -0800

    CLEAN!!

---
+++ ShoutOut/ShoutCreateViewController.m
@@ -0,0 +1,67 @@
+//
+//  ShoutCreateViewController.m
+//  ShoutOut
+//
+//  Created by Kevin Ho on 3/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShoutCreateViewController.h"
+
+@implementation ShoutCreateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+    UIImage *imageOfEvent = [UIImage imageNamed:@"notuploaded.png"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:imageOfEvent];
+    imageView.frame = CGRectMake(50, 50, imageOfEvent.size.width, imageOfEvent.size.height);
+    [self.view addSubview:imageView];
+    
+    UITextField * textField = [[UITextField alloc] initWithFrame:CGRectMake(30, 30, 100, 100)];
+    [textField setText:@"Hello World"];
+    [self.view addSubview:textField];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
